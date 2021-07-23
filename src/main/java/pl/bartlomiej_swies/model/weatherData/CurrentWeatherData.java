ca6742d5863bd316bc32804763a3a8ca3cf6e81d
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-23 19:47:00 +0200

    Current weather data class created

---
+++ src/main/java/pl/bartlomiej_swies/model/weatherData/CurrentWeatherData.java
@@ -0,0 +1,21 @@
+package pl.bartlomiej_swies.model.weatherData;
+
+public class CurrentWeatherData {
+
+    private double latitude;
+    private double longitude;
+
+    private String weatherDescription;
+    private String weatherIcon;
+
+    private double temperature;
+    private int pressure;
+    private int humidity;
+
+    private double windSpeed;
+    private int windDegree;
+
+    private int cloudsCover;
+
+
+}
