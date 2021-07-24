8cb7ea8e7c51e1dce3e2ae9724a5f67a27008fd4
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-24 10:44:50 +0200

    Abstract class for retrieving data from Json

---
+++ src/main/java/pl/bartlomiej_swies/model/weatherData/WeatherData.java
@@ -0,0 +1,41 @@
+package pl.bartlomiej_swies.model.weatherData;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public abstract class WeatherData {
+
+    private JSONObject jsonObject;
+
+    public WeatherData(String jsonString) {
+        this.jsonObject = new JSONObject(jsonString);
+    }
+
+    protected String getStringValueFromJsonObject(String object, String key) {
+        String stringValue = jsonObject.getJSONObject(object).getString(key);
+        return stringValue;
+    }
+
+    protected double getDoubleValueFromJsonObject(String object, String key) {
+        double doubleValue = jsonObject.getJSONObject(object).getDouble(key);
+        return doubleValue;
+    }
+
+    protected String getStringValueFromJsonArray(String object, String key) {
+        JSONArray jsonArray = jsonObject.getJSONArray(object);
+        for(int i = 0; i < jsonArray.length(); i++){
+            String stringValue = jsonArray.getJSONObject(i).getString(key);
+            return stringValue;
+        }
+        return null;
+    }
+
+    protected Double getDoubleValueFromJsonArray(String object, String key) {
+        JSONArray jsonArray = jsonObject.getJSONArray(object);
+        for(int i = 0; i < jsonArray.length(); i++){
+            double doubleValue = jsonArray.getJSONObject(i).getDouble(key);
+            return doubleValue;
+        }
+        return null;
+    }
+}
