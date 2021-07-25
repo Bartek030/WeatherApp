9ed74cbf9a5cff2d4d6f628cf51d98d22bd9f8b9
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-25 23:09:18 +0200

    Sending data retrieved from API to the view

--- src/main/java/pl/bartlomiej_swies/model/OneDayDataForDailyForecast.java
+++ src/main/java/pl/bartlomiej_swies/model/OneDayDataForDailyForecast.java
@@ -1,6 +1,5 @@
 package pl.bartlomiej_swies.model;
 
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,36 +8,56 @@ import javafx.scene.layout.VBox;
 import pl.bartlomiej_swies.model.weatherData.DailyForecast;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class OneDayDataForDailyForecast {
 
-    private VBox weatherData = new VBox();
-    private Label dayOfTheWeek = new Label();
-    private Label date = new Label();
-    private HBox windCondition = new HBox();
-    private Label windFieldName = new Label();
-    private ImageView windArrow = new ImageView();
-    private Label windspeed = new Label();
-   // private ImageView weatherIcon = new ImageView();
+    private DailyForecast dailyForecast;
 
+    private VBox weatherData = new VBox();
+    private Label dayOfTheWeek;
+    private Label dateOfTheDay;
+    private HBox windCondition = new HBox();
+    private Label windFieldName = new Label("wiatr");
+    private ImageView windArrow = new ImageView(getWeatherWindImage());
+    private Label windspeed;
+    private ImageView weatherIcon;
 
     public OneDayDataForDailyForecast(DailyForecast dailyForecast) {
-        dayOfTheWeek.setText("wtorek");
-        date = new Label("05-07-2020");
-        windFieldName = new Label("wiatr");
-        windArrow = new ImageView();
-        windspeed = new Label("5,55 m/s");
-        setWeatherWindImage();
+        this.dailyForecast = dailyForecast;
+        setDateInfo();
+        setWindConditions();
+        setWeatherIcon();
         windCondition.getChildren().addAll(windFieldName, windArrow, windspeed);
-        weatherData.getChildren().addAll(dayOfTheWeek, date, windCondition);
+        weatherData.getChildren().addAll(dayOfTheWeek, dateOfTheDay, windCondition, weatherIcon);
     }
 
-    private void setWeatherWindImage() {
+    private void setDateInfo() {
+        Date date = new Date(dailyForecast.getDt() * 1000);
+        String dzientygodnia = new SimpleDateFormat("EEEE").format(date);
+        String dateInStringFormat = new SimpleDateFormat("dd-MM-YYYY").format(date);
+
+        dayOfTheWeek = new Label(dzientygodnia);
+        dateOfTheDay = new Label(dateInStringFormat);
+    }
+
+    private void setWindConditions() {
+        windspeed = new Label(dailyForecast.getWind_speed() + " m/s");
+        windArrow.setRotate(dailyForecast.getWind_deg());
+    }
+
+    private void setWeatherIcon() {
+        String imageIcon = dailyForecast.getWeather().get(0).getIcon();
+        String imageSource = "http://openweathermap.org/img/wn/" + imageIcon + "@2x.png";
+        weatherIcon = new ImageView(new Image(imageSource));
+    }
+
+    private Image getWeatherWindImage() {
         URL url = getClass().getResource("/view/img/arrow.png");
         Image image = new Image(String.valueOf(url));
-        this.windArrow.setImage(image);
-        this.windArrow.setRotate(160);
+        return image;
     }
 
     public VBox getWeatherData() {
