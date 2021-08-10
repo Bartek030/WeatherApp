1727fed34ee60a6fe473a97fa96e12a8b6ded2b6
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-08-10 22:27:15 +0200

    Code review - Fields in classes with weather data are now final

--- src/main/java/pl/bartlomiej_swies/model/DailyForecastData.java
+++ src/main/java/pl/bartlomiej_swies/model/DailyForecastData.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class DailyForecastData {
 
-    private List<DailyForecast> daily;
+    private final List<DailyForecast> daily;
 
     public DailyForecastData(List<DailyForecast> daily) {
         this.daily = daily;
